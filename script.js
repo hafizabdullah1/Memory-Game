8071ced843de7113d94b742f4f80abc813e1cc94
Abdullah2723 <127143084+Abdullah2723@users.noreply.github.com>
2024-09-16 15:39:13 +0500

    make some changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("/memory-game/service-worker.js")
       .then((registration) => {
         console.log(
           "Service Worker registered with scope:",
